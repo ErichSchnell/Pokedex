efe8bbcfccb64b1da69a980fa0551bc7792a47fc
Jere Schneider <jereschneider8@gmail.com>
2023-11-11 00:29:51 -0300

    create project

---
+++ app/build.gradle.kts
@@ -0,0 +1,67 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.com.android.application)
+    alias(libs.plugins.org.jetbrains.kotlin.android)
+}
+
+android {
+    namespace = "com.jereschneider.pokedex"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.jereschneider.pokedex"
+        minSdk = 26
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.3"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.core.ktx)
+    implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.bundles.compose.ui)
+    implementation(platform(libs.compose.bom))
+    implementation(libs.material3)
+
+    testImplementation(libs.junit)
+
+    androidTestImplementation(libs.bundles.android.testing)
+    androidTestImplementation(platform(libs.compose.bom))
+
+    debugImplementation(libs.bundles.compose.ui.debug)
+}
